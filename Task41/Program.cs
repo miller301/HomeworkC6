72ce14d6b966034d62c1164a796ecea969649b46
miller301 <perepelkin2241@yandex.ru>
2022-12-11 20:45:06 +0300

    hw is done

---
+++ Task41/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Введите желаемое количество чисел");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] mass(int size)
+{
+    int[] array = new int[size];
+    Console.WriteLine("Введите заданное количество чисел по очереди");
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return array;
+}
+void posi(int[] array)
+{
+    int pos = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0) pos = pos + 1;
+    }
+    Console.WriteLine($"Количество чисел больше нуля {pos}");
+}
+
+int[] array = mass(size);
+posi(array);
+
+
---
+++ Task43/Program.cs
@@ -0,0 +1,17 @@
+﻿void point(double k1, double b1, double k2, double b2)
+{
+    double x = (b1 - b2) / (k2 - k1);
+    double y = (k2 * b1 - k1 * b2) / (k2 - k1);
+    if (k1 == k2) Console.WriteLine("Прямые параллельны");
+    else Console.Write($"Координаты точки пересечения заданных прямых ({x}; {y})");
+}
+
+Console.WriteLine("Введите значение b1 ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите значение k1 ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите значение b2 ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите значение k2 ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+point(k1, b1, k2, b2);
